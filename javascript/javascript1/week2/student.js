e103e0277c78ea0452cabd875b3cdb3b7c338919
RemyaManoharan <remyajinil@outlook.com>
2023-03-07 16:58:52 +0100

    combine two condition based onfeedback

--- javascript/javascript1/week2/student.js
+++ javascript/javascript1/week2/student.js
@@ -2,28 +2,25 @@ const class07Students = [];
 
 function addStudentToClass(studentName) {
   //call function to get the strength of class
-  let strength = getNumberOfStudents(class07Students);
-  //console.log("strength of class is " + strength);
+  let capacity = getNumberOfStudents(class07Students);
+  //console.log("strength of class is " + capacity);
 
-  if (strength >= 7 && studentName.toUpperCase() != "QUEEN") {
+  if (capacity >= 7 && studentName.toUpperCase() !== "QUEEN") {
     console.log("Cannot add more students to class 07");
     return class07Students;
   } else if (class07Students.includes(studentName.toUpperCase())) {
     return `Student ${studentName} is already in the class`;
   } else if (!studentName) {
     return "cannot add empty string";
-  } else if (studentName.toUpperCase() === "QUEEN") {
-    class07Students.push(studentName.toUpperCase());
-    return class07Students;
-  } else if (strength < 7) {
+  } else if (capacity < 7 || studentName.toUpperCase() === "QUEEN") {
     class07Students.push(studentName.toUpperCase());
     return class07Students;
   }
 }
 function getNumberOfStudents(class07Students) {
-  let strength = class07Students.length;
-  console.log(strength);
-  return strength;
+  let capacity = class07Students.length;
+  console.log(capacity);
+  return capacity;
 }
 
 console.log(addStudentToClass("aarav"));
@@ -38,4 +35,4 @@ console.log(addStudentToClass("Meena"));
 console.log(addStudentToClass("John"));
 console.log(addStudentToClass("Albert"));
 console.log(addStudentToClass("Queen"));
-// console.log(addStudentToClass("Jenni"));
+// console.log(addStudentToClass("Guna"));
