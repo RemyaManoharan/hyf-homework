4ed9e00cee62821e5872e173af694f9d3f10dba2
RemyaManoharan <remyajinil@outlook.com>
2023-03-07 16:56:20 +0100

    added condition for null feild

--- javascript/javascript1/week2/fullname.js
+++ javascript/javascript1/week2/fullname.js
@@ -1,21 +1,28 @@
 const getFullname = (firstName, surName, useFormalName) => {
-  if (useFormalName) {
-    return `Lord ${firstName} ${surName}`;
-  } else return `${firstName} ${surName}`;
+  if (!firstName || firstName === " " || !surName || surName === " ") {
+    return "please provide your name";
+  } else {
+    if (useFormalName) {
+      return `Lord ${firstName} ${surName}`;
+    } else return `${firstName} ${surName}`;
+  }
 };
-
-console.log(getFullname("James", "Mary", true));
+console.log(getFullname("John", "Mary", true));
 console.log(getFullname("Rosh", "Susan", false));
-console.log(getFullname("James", "Mary", true));
+console.log(getFullname(" ", "Mary", true));
 
 // resolving the above code  for women
 
 const getFullnameW = (firstName, surName, useFormalName, gender) => {
-  if (useFormalName && gender === "male") {
-    return `Lord ${firstName} ${surName}`;
-  } else if (useFormalName && gender === "female") {
-    return `Lady ${firstName} ${surName}`;
-  } else return `${firstName} ${surName}`;
+  if (!firstName || firstName === " " || !surName || surName === " ") {
+    return "please provide your name";
+  } else {
+    if (useFormalName && gender === "male") {
+      return `Lord ${firstName} ${surName}`;
+    } else if (useFormalName && gender === "female") {
+      return `Lady ${firstName} ${surName}`;
+    } else return `${firstName} ${surName}`;
+  }
 };
 
 console.log(getFullnameW("Rosh", "Susan", true, "female"));
