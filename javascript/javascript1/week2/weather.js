3f2a655fb349a7addbef78c577761e8cfedf6438
RemyaManoharan <remyajinil@outlook.com>
2023-03-07 16:57:31 +0100

    added condition for temp greater than 100'C

--- javascript/javascript1/week2/weather.js
+++ javascript/javascript1/week2/weather.js
@@ -1,5 +1,7 @@
-function clothesBaseTemperature(temperature) {
-  if (temperature <= 20) {
+function getClothesBasedOnTemperature(temperature) {
+  if (temperature <= -20) {
+    return "Its very cold outside!Wear 3 layer  clothes!";
+  } else if (temperature <= 20) {
     return "Jackets and thermals";
   } else if (temperature > 20 && temperature < 25) {
     return "Denim jacket and Jeans";
@@ -7,8 +9,10 @@ function clothesBaseTemperature(temperature) {
     return "Short sleeve Tshirts and Pants";
   } else if (temperature >= 40) {
     return "cotton clothes and linen";
+  } else if (temperature >= 100) {
+    return "High temperature!Please stay at home.Wear light cotton dress ";
   }
 }
 
-const clothesToWear = clothesBaseTemperature(45);
+const clothesToWear = getClothesBasedOnTemperature(45);
 console.log(clothesToWear);
