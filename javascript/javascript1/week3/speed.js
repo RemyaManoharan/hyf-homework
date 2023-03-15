38d0d9df4eba482852014bd4738d2ce551e75792
RemyaManoharan <remyajinil@outlook.com>
2023-03-15 17:52:06 +0100

    When will be there

--- javascript/javascript1/week3/speed.js
+++ javascript/javascript1/week3/speed.js
@@ -1,12 +1,12 @@
 const travelInformation = {
-    speed: 50,
-    destinationDistance: 432,
-  };
-  function findTravelTime(travelInformation) {
-    const time = travelInformation.destinationDistance / travelInformation.speed;
-    const hour = Math.floor(time);
-    const minutes = Math.floor((time - hour) * 60);
-    return `${hour} hours and ${minutes} minutes`;
-  }
-  const travelTime = findTravelTime(travelInformation);
-  console.log(travelTime); 
+  speed: 50,
+  destinationDistance: 432,
+};
+function findTravelTime(travelInformation) {
+  const time = travelInformation.destinationDistance / travelInformation.speed;
+  const hour = Math.floor(time);
+  const minutes = Math.floor((time - hour) * 60);
+  return `${hour} hours and ${minutes} minutes`;
+}
+const travelTime = findTravelTime(travelInformation);
+console.log(travelTime);
